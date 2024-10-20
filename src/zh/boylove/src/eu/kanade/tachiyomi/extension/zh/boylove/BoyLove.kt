0a4a565fc1c2e32ee251568e9ecfe97c58a80390
oCameLo <92753+oTnTh@users.noreply.github.com>
2024-10-21 04:31:33 +0800

    Update mirrors url

--- src/zh/boylove/src/eu/kanade/tachiyomi/extension/zh/boylove/BoyLove.kt
+++ src/zh/boylove/src/eu/kanade/tachiyomi/extension/zh/boylove/BoyLove.kt
@@ -207,7 +207,9 @@ class BoyLove : HttpSource(), ConfigurableSource {
 
         // redirect URL: https://fuhouse.club/bl
         // link source URL: https://boylovepage.github.io/boylove_page
-        private val MIRRORS get() = arrayOf("fufun.cloud", "fufun.club", "boylove1.mobi", "boylove3.cc", "boylove.cc", "boyloves.space", "boylove4.xyz", "boyloves.fun", "boylove.today", "fuzai.one", "xxfuzai.xyz", "fuzai.cc")
-        private val MIRRORS_DESC get() = arrayOf("fufun.cloud", "fufun.club", "boylove1.mobi", "boylove3.cc", "boylove.cc（非大陆）", "boyloves.space", "boylove4.xyz", "boyloves.fun", "boylove.today", "fuzai.one", "xxfuzai.xyz", "fuzai.cc（非大陆）")
+        //  "boylove1.mobi", "boylove3.cc", "boyloves.space", "boyloves.fun", "boylove.today", "fuzai.one", "xxfuzai.xyz", "fuzai.cc"
+        private val MIRRORS get() = arrayOf("fufun.cloud", "fufun.club", "boylove.cc", "boylove4.xyz")
+        //   "boylove1.mobi", "boylove3.cc", "boyloves.space", "boyloves.fun", "boylove.today", "fuzai.one", "xxfuzai.xyz", "fuzai.cc（非大陆）"
+        private val MIRRORS_DESC get() = arrayOf("fufun.cloud", "fufun.club", "boylove.cc（非大陆）", "boylove4.xyz")
     }
 }
